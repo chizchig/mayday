d56c040307eac3627a9233e3e1f4dc00dba96bc7
chinazam C. Dennar <exchangehub@aol.com>
2023-12-08 13:38:00 -0700

    original commit

---
+++ gke.tf
@@ -0,0 +1,13 @@
+
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster
+  location = var.region
+
+  initial_node_count = 3
+
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "cluster" {
+  type = string
+  default = "gulf_cluster"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "project_id" {
+  type = string
+  default = "roar1984"
+}
