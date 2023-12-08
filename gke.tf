d6c31695c1aefdda006ca750b1676eb34068077c
chinazam C. Dennar <exchangehub@aol.com>
2023-12-08 16:02:25 -0700

    flash commit

--- gke.tf
+++ gke.tf
@@ -4,7 +4,7 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster
   location = var.region
 
-  initial_node_count = 3
+  initial_node_count = 2
 
   node_config {
     machine_type = "n1-standard-1"
